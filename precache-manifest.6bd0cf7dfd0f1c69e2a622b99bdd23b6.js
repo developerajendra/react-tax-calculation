85e802243dded9dceb45bb4860a09a35a126612f
Rajendra <rajendra@Rajendras-MacBook-Pro.local>
2020-04-14 22:07:20 +0530

    Updates

---
+++ precache-manifest.6bd0cf7dfd0f1c69e2a622b99bdd23b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64ec4fdda32471315095f2208200852",
+    "url": "/react-tax-calculation/index.html"
+  },
+  {
+    "revision": "a127f115a79a878ad01b",
+    "url": "/react-tax-calculation/static/css/main.cdf4031a.chunk.css"
+  },
+  {
+    "revision": "98dd2d6565e8c484c023",
+    "url": "/react-tax-calculation/static/js/2.5e318271.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tax-calculation/static/js/2.5e318271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a127f115a79a878ad01b",
+    "url": "/react-tax-calculation/static/js/main.f1e1db40.chunk.js"
+  },
+  {
+    "revision": "0f160576d5c72eb698fd",
+    "url": "/react-tax-calculation/static/js/runtime-main.51a8b3a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tax-calculation/static/media/logo.5d5d9eef.svg"
+  }
+]);
